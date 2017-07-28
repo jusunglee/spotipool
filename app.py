47210850bc0cf410719d54a8e22d9eec9ac57b20
Jusung Lee <e.jusunglee@gmail.com>
2017-07-27 21:06:47 -0700

    flask setup

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask
-from datetime import datetime
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
-    return """
-    <h1>Spotipool</h1>
-    <p>It is currently {time}.</p>
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
+def index():
+    user = {'name': 'jusung'}
+    title = "jusung's world"
+    data = {
+        'title': title,
+        'user': user
+    }
+    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
