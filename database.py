73a66de1c0bf946ae16081887c6eb33865f0f80d
Anthony Chan <alc1@andrew.cmu.edu>
2017-08-02 00:23:55 -0500

    Offloaded key loading to a seperate file and added sign in function to return user tokens

--- database.py
+++ database.py
@@ -18,11 +18,26 @@ def push(db1, user, key, val):
      to the given database. No return value"""
     db1.child(key).set(val, user['idToken'])
 
+def signin(fb1):
+    '''signin returns the user token for the given firebase instance'''
+    #load in the firebase authentication info from a file
+    keys = "keys/db_auth.txt"
+    with open(keys) as file_:
+        content = file_.readlines()
+
+    authinfo = dict()
+
+    for line in content:
+        tokens = line.split('=')
+        tokens = [token.strip() for token in tokens]
+        authinfo[tokens[0]] = tokens[1]
+
+    auth = fb1.auth()
+    return auth.sign_in_with_email_and_password(authinfo["email"], authinfo["password"])
+
 def testdatabase():
     '''testDB tests posting to the firebase database'''
-    fb1 = firebase()
-    auth = fb1.auth()
-    user = auth.sign_in_with_email_and_password("test@spotipool.com", "spotipool")
+    user = signin(firebase())
     push(firebase().database(), user, "test", "value")
 
 testdatabase()
