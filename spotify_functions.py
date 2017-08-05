4a75412ee1ffcdabee5db09f2cbefd01a6478609
Anthony Chan <alc1@andrew.cmu.edu>
2017-08-05 14:56:59 -0500

    Forced `add_tracks_to_playlist` to return values

--- spotify_functions.py
+++ spotify_functions.py
@@ -86,5 +86,4 @@ def get_tracks_from_string_query(sp, string_query):
 def add_track_to_playlist(username,track_id,playlist_id):
     sp = load_spotipy_object(load_keys)
     mod_track_uri = ['spotify:track:'+track_id]
-    results = sp.user_playlist_add_tracks(username, playlist_id, mod_track_uri)
-    print(results)
+    return  sp.user_playlist_add_tracks(username, playlist_id, mod_track_uri)
