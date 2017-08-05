52c7f3b934c88a33d1ce66b3b5056fc620850ebe
Jusung Lee <e.jusunglee@gmail.com>
2017-08-04 20:32:47 -0400

    Added basic page with search bar

--- static/scripts/spotifyRequests.js
+++ static/scripts/spotifyRequests.js
@@ -1,6 +1,8 @@
-sendTrackSearchRequest = function(form) {
-    console.log(form);
-    alert("hello");
+sendTrackSearchRequest = function() {
+    // alert("hello");
+    // var searchBarText = document.getElementById("searchBar").value;
+    // console.log(searchBarText);
+    console.log("done");
     // $.get("demo_test.asp/", function (data, status) {
     //     alert("Data: " + data + "\nStatus: " + status);
     // });
