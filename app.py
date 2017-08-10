3b1ef46996dae3af6bcfe1767b66a6b1d18695a7
Jusung Lee <e.jusunglee@gmail.com>
2017-08-10 15:01:00 -0700

    Basic search and results for song in /dashboard

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import spotify_functions as sf
 import database as db
 
 app = Flask(__name__)
-
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route('/')
 def index():
--- static/scripts/spotifyRequests.js
+++ static/scripts/spotifyRequests.js
@@ -1,7 +1,31 @@
-sendTrackSearchRequest = function() {
-    var searchBarText = document.getElementById("searchBar").value;
-    searchBarText = encodeURI(searchBarText);
-    $.get("track/" + searchBarText, function (rawData, status) {
-        return data;
+$(document).ready(function(){
+    $("#search-button").click(function() {
+        var query = $("#search-bar")[0].value;
+        encodedQuery = encodeURI(query);
+        $.get("track/" + encodedQuery, function (data, status) {
+            displaySearchResults(data);
+        });
     });
-};
+});
+
+displaySearchResults = function(searchResults) {
+    console.log(searchResults);
+    for (var i = 0; i < searchResults.length; i++) {
+        var track = searchResults[i];
+        var trackName = track['song_name'];
+        var artistName = track['artist_name'];
+        var songId = track['song_id'];
+        var $resultItemDiv = $("<div>", {id: "search-result-" + i, "class": "search-result-item"});
+        var $trackNameItem = $("<p>", {"class": "track-name"});
+        $trackNameItem.append(trackName);
+        var $artistNameItem = $("<p>", {"class": "artist-name"});
+        $artistNameItem.append(artistName);
+        $resultItemDiv.append($trackNameItem);
+        $resultItemDiv.append($artistNameItem);
+        $resultItemDiv.click(function(){
+            console.log(songId);
+        });
+
+        $("#search-results-container").append($resultItemDiv);
+    }
+}
